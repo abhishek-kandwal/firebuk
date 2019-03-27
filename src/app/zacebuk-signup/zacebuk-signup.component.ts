603ae7ee406f402355648824e70fc4408cfeca70
srishtikapoor <k.srishti99@gmail.com>
2019-03-27 19:33:19 +0530

    Updated sign up

--- src/app/zacebuk-signup/zacebuk-signup.component.ts
+++ src/app/zacebuk-signup/zacebuk-signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-zacebuk-signup',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./zacebuk-signup.component.css']
 })
 export class ZacebukSignupComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+  employeeForm:FormGroup;
+  constructor(private fb:FormBuilder){}
+ 
+   ngOnInit() {
+     this.employeeForm= this.fb.group({
+       //second arguements are sync validations, async are passed as third arguement(returns promises/observables)
+ 
+       fullName:[' ', [Validators.required, Validators.minLength(2), Validators.maxLength(15)]],  
+       email:[' ',[Validators.required, Validators.email]],
+       password:[' ', [Validators.required, Validators.minLength(2), Validators.maxLength(15)]]
+       
+     });
+    
+   }
+   onSubmit():void{
+     console.log(this.employeeForm.value);
+   }
 
 }
