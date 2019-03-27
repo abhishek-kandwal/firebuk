cf993a774bcd96d1afcb7499cb202ee9b6d5f07b
Papu Kumar <papuruth@gmail.com>
2019-03-27 20:54:08 +0530

    Integrated post function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { ZacebukHeaderComponent } from './zacebuk-header/zacebuk-header.component';
 import { ZacebukWallComponent } from './zacebuk-wall/zacebuk-wall.component';
@@ -10,12 +9,26 @@ import { ZacebukFooterComponent } from './zacebuk-footer/zacebuk-footer.componen
 import { Routes, RouterModule } from '@angular/router';
 import { ZacebukUsrProfileComponent } from './zacebuk-usr-profile/zacebuk-usr-profile.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { PostdataService } from './post-data.service';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpErrorHandler } from './http-error-handler.service';
+import { MessageService } from './message.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const appRoutes: Routes = [
   { path: '', component: ZacebukWallComponent },
   { path: 'app-zacebuk-login', component: ZacebukLoginComponent },
   { path: 'app-zacebuk-signup', component: ZacebukSignupComponent }
 ]
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCoBKG6stqxU9KwS5EsMqDrppApRYW6X20',
+  authDomain: 'reactiveform-ffa7e.firebaseapp.com',
+  databaseURL: 'https://reactiveform-ffa7e.firebaseio.com',
+  projectId: 'reactiveform-ffa7e',
+  storageBucket: 'reactiveform-ffa7e.appspot.com',
+  messagingSenderId: '236575886616'
+};
 
 @NgModule({
   declarations: [
@@ -30,10 +43,11 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot(appRoutes),
-    ReactiveFormsModule
+    ReactiveFormsModule, BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [PostdataService, HttpErrorHandler, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/zacebuk-signup/zacebuk-signup.component.ts
+++ src/app/zacebuk-signup/zacebuk-signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { PostdataService } from '../post-data.service';
 
 @Component({
   selector: 'app-zacebuk-signup',
@@ -7,22 +8,38 @@ import { FormGroup, FormControl, Validators, FormBuilder} from '@angular/forms';
   styleUrls: ['./zacebuk-signup.component.css']
 })
 export class ZacebukSignupComponent implements OnInit {
-  employeeForm:FormGroup;
-  constructor(private fb:FormBuilder){}
- 
-   ngOnInit() {
-     this.employeeForm= this.fb.group({
-       //second arguements are sync validations, async are passed as third arguement(returns promises/observables)
- 
-       fullName:[' ', [Validators.required, Validators.minLength(2), Validators.maxLength(15)]],  
-       email:[' ',[Validators.required, Validators.email]],
-       password:[' ', [Validators.required, Validators.minLength(2), Validators.maxLength(15)]]
-       
-     });
-    
-   }
-   onSubmit():void{
-     console.log(this.employeeForm.value);
-   }
+  employeeForm: FormGroup;
+  users = [];
+  constructor(private fb: FormBuilder, private postData: PostdataService) { }
+
+  ngOnInit() {
+    this.employeeForm = this.fb.group({
+      // second arguements are sync validations, async are passed as third arguement(returns promises/observables)
+      fullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(15)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(15)]],
+      phone: [''],
+      gender: ['']
+    });
+
+  }
+  get fieldValues() {
+    return this.employeeForm.controls;
+  }
+  onSubmit(): void {
+    console.log(this.employeeForm.value);
+    const {fullName, email, password, phone, gender} = this.employeeForm.value;
+    console.log(fullName);
+    if (gender === 'male') {
+      const formRequest = {fullName, email, password, phone, gender};
+      this.postData.addUser(formRequest)
+      .subscribe(user => this.users.push(user));
+    } else {
+      const formRequest = {fullName, email, password, phone, gender};
+      this.postData.addUser(formRequest)
+      .subscribe(user => this.users.push(user));
+    }
+    this.employeeForm.reset();
+  }
 
 }
