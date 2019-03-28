95be83ae00fc74c9929604038604861a76e8d01b
devesh166 <jiodevesh@gmail.com>
2019-03-28 22:30:28 +0530

    added fetch

--- src/app/zacebuk-wall/zacebuk-wall.component.ts
+++ src/app/zacebuk-wall/zacebuk-wall.component.ts
@@ -8,16 +8,27 @@ import { FetchJsonDataService } from '../fetch-json-data.service';
   styleUrls: ['./zacebuk-wall.component.css']
 })
 export class ZacebukWallComponent implements OnInit {
- postdata;
+  postdata;
+  postList=[];
+  //counter;
+  postValue=[];
   constructor(private _postsData: FetchJsonDataService) { }
-
+  counter;
   ngOnInit() {
-    // this._postsData.getJsonData()
-    //   .subscribe(data => {
-
-    //     this.postdata = data;
-    //     console.log(this.postdata);
-    //     });
+    this._postsData.getPost()
+    .subscribe(data => {
+      let temp;
+      this.postList = Object.keys(data);
+      this.postList.map((el,index)=>{
+        temp = data[el]['Post_content'];
+        this.postValue.push(temp);
+        console.log(this.postList);
+        console.log(this.postValue);
+        
+      })
+            
+      });
+      
   }
   // to get json data. anyone who changes kindly change the function name.
   // samplefn(){
