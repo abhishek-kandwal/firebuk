b6a4753ffe6dc313b2d2d4e643d7f98b0282caef
srishtikapoor <k.srishti99@gmail.com>
2019-03-28 19:13:19 +0530

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule } from '@angular/common/http';
-
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
 import { ZacebukHeaderComponent } from './zacebuk-header/zacebuk-header.component';
 import { ZacebukWallComponent } from './zacebuk-wall/zacebuk-wall.component';
@@ -29,10 +26,8 @@ const appRoutes: Routes = [
     ZacebukSignupComponent,
     ZacebukLoginComponent,
     ZacebukFooterComponent,
-    HttpClientModule,
     ZacebukUsrProfileComponent,
   ],
-  
   imports: [
     BrowserModule,
     HttpClientModule,
